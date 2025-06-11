b427babec269d104bbc7182f31b5df6245214af1
Noam Yan <noamyan9@gmail.com>
2025-06-10 19:16:05 -0500

    Build Contact component. Add messageSlice

---
+++ src/redux/messageSlice/message.thunks.ts
@@ -0,0 +1,53 @@
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import {handleThunkAxiosError} from "../../shared/utils/thunkErrorHandlers.ts";
+import axios from "axios";
+import type {ContactFormData} from "../../shared/models/IMessage.ts";
+
+export const sendMessage = createAsyncThunk(
+    'messages/send',
+    async (contactForm: ContactFormData, thunkAPI) => {
+        try {
+            await axios.post(`${process.env.BACKEND_API}/messages`, {
+                method: 'POST',
+                body: contactForm,
+            });
+        } catch (error) {
+            return handleThunkAxiosError(error, thunkAPI);
+        }
+    }
+)
+
+export const getAllMessages = createAsyncThunk(
+    'messages/getAll',
+    async (_, thunkAPI) => {
+        try {
+            const response = await axios.get(`${process.env.BACKEND_API}/messages`, {withCredentials:true});
+            return response.data;
+        } catch (error) {
+            return handleThunkAxiosError(error, thunkAPI);
+        }
+    }
+)
+
+export const getMessage = createAsyncThunk(
+    'messages/getOne',
+    async (id: number, thunkAPI) => {
+        try {
+            const response = await axios.get(`${process.env.BACKEND_API}/messages/${id}`, {withCredentials:true});
+            return response.data;
+        } catch (error) {
+            return handleThunkAxiosError(error, thunkAPI);
+        }
+    }
+)
+
+export const solvedMessage = createAsyncThunk(
+    'messages/solve',
+    async (id: number, thunkAPI) => {
+        try {
+            await axios.patch(`${process.env.BACKEND_API}/messages/${id}`, null, {withCredentials:true});
+        } catch (error) {
+            return handleThunkAxiosError(error, thunkAPI);
+        }
+    }
+)
---
+++ src/shared/models/IMessage.ts
@@ -0,0 +1,17 @@
+export interface IMessage {
+    id: string;
+    email: string;
+    subject: string;
+    content: string;
+    status: MessageStatus;
+    createdAt: Date;
+    updatedAt: Date;
+    updatedBy: string;
+}
+type MessageStatus = 'SOLVED' | 'UNSOLVED';
+
+export interface ContactFormData {
+    email: string;
+    subject: string;
+    message: string;
+}
