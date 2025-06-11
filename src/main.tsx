8d80ec923bd092f08565dd96d85d48ff5a18433e
Noam Yan <noamyan9@gmail.com>
2025-06-10 19:15:36 -0500

    Add redux store provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,23 +5,29 @@ import App from './App.tsx'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {AlertProvider} from "./components/alert/AlertHook.tsx";
 import AuthGuard from "./guards/AuthGuard.tsx";
+import Contact from "./pages/contact/Contact.tsx";
+import store from "./redux/store.ts";
+import {Provider} from "react-redux";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <BrowserRouter>
-          <AlertProvider>
-              <Routes>
-                  {/*pages without Nav component*/}
-                  <Route path="/" element={<App />} >
-                      {/*pages with Nav component*/}
-                      <Route element={<AuthGuard/>}>
-                          {/*guarded pages*/}
-                      </Route>
-                      {/*public pages*/}
+      <Provider store={store}>
+          <BrowserRouter>
+              <AlertProvider>
+                  <Routes>
+                      {/*pages without Nav component*/}
+                      <Route path={'/contact'} element={<Contact />}/>
+                      <Route path="/" element={<App />} >
+                          {/*pages with Nav component*/}
+                          <Route element={<AuthGuard/>}>
+                              {/*guarded pages*/}
+                          </Route>
+                          {/*public pages*/}
 
-                  </Route>
-              </Routes>
-          </AlertProvider>
-      </BrowserRouter>
+                      </Route>
+                  </Routes>
+              </AlertProvider>
+          </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 import authReducer from './authSlice/auth.slice.ts';
+import messageReducer from './messageSlice/message.slice.ts';
 
 const store = configureStore({
     reducer:{
         auth: authReducer,
+        message: messageReducer
     },
     devTools: true,
     middleware: (getDefaultMiddleware)=>getDefaultMiddleware()
